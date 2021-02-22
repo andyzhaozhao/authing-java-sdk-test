6a71c6fa811ca09198da17b27f24ba1f33d15b1f
zhaozhao <zhaozhao@mail.taiji.com.cn>
2021-02-22 16:42:47 +0800

    Add:init project

---
+++ src/test/java/com/example/authingjavasdktest/AuthingJavaSdkTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authingjavasdktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthingJavaSdkTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
