2e19c8f5618e0309b92f46b5110c0d3393b4a4a3
zhaozhao <zhaozhao@mail.taiji.com.cn>
2021-03-14 15:11:44 +0800

    Mod: SDK

--- src/main/java/com/example/authingjavasdktest/api/ApplicationManagementClientTestController.java
+++ src/main/java/com/example/authingjavasdktest/api/ApplicationManagementClientTestController.java
@@ -1,9 +1,11 @@
 package com.example.authingjavasdktest.api;
 
+import cn.authing.core.auth.AuthenticationClient;
 import cn.authing.core.graphql.GraphQLException;
 import cn.authing.core.mgmt.ApplicationManagementClient;
 import cn.authing.core.mgmt.ManagementClient;
 import cn.authing.core.types.Application;
+import cn.authing.core.types.LoginByUsernameInput;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +27,17 @@ public class ApplicationManagementClientTestController {
 
         List<Application> result = this.applicationManagementClient.listApplications().execute();
         return result;
+    }
+
+    @GetMapping(path = "/lt")
+    public Boolean lt(String menuId) throws IOException, GraphQLException {
+        AuthenticationClient authenticationClient = new AuthenticationClient("5f9d0cee4a8f5e150cf6470d");
+        authenticationClient.setHost("https://core.authing.cn");
+        String username = "test";
+        String password = "andy123456";
+        authenticationClient.loginByUsername(new LoginByUsernameInput(username, password)).execute();
+
+        authenticationClient.logout().execute();
+        return true;
     }
 }
